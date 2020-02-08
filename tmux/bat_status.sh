34757dc2912c4aa6c512e8dc39f2814fb6327c0d
Ashfaqur Rahman <sajib.finix@gmail.com>
2020-02-08 10:37:31 -0800

    Add percentage

--- tmux/bat_status.sh
+++ tmux/bat_status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-printf "%s %s" `acpi | awk '{print substr($4, 1, length($4)-1)}'`\
+printf "%s %s" `acpi | awk '{print substr($4, 1, length($4)-1) "%"}'`\
 	`acpi -a | awk '{print $3}'`
