f5a0206912f4a9b90e585c02ef20d110de4bb858
Ashfaqur Rahman <sajib.finix@gmail.com>
2020-02-07 22:51:34 -0800

    detect wifi interface name automatically

--- tmux/net_status.sh
+++ tmux/net_status.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-iw dev wlp3s0 link | awk '{if ($1 == "SSID:") print $2}'
+interface=`ip a | awk '/^[0-9]/{if ($2 ~ "wlp") print substr($2, 1, length($2))}'`
+iw dev $interface link | awk '{if ($1 == "SSID:") print $2}'
