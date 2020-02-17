0e63b6c56d0bf387f85e33a5ae868c0de7f85d01
Ashfaqur Rahman <sajib.finix@gmail.com>
2020-02-16 20:14:19 -0800

    bugfix in disk and net

--- tmux/disk_status.sh
+++ tmux/disk_status.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-df -h | awk '{if ($6 == "/") root=$4; if ($6 == "/home") home=$4} END{print "/: " root; if (home != 0) print " ~:" home}'
+df -h | awk '{if ($6 == "/") root=$4; if ($6 == "/home") home=$4} END{if (home != 0) print "/: " root " ~:" home; else print "/: " root}'
--- tmux/net_status.sh
+++ tmux/net_status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-interface=`ip a | awk '/^[0-9]/{if ($2 ~ "wlp") print substr($2, 1, length($2))}'`
-iw dev $interface link | awk '/^Not/{print "W: Not Connected"}{if ($1 == "SSID:") print "W: " $2}'
+interface=`/sbin/ip a | awk '/^[0-9]/{if ($2 ~ "wlp") print substr($2, 1, length($2))}'`
+/sbin/iw dev $interface link | awk '/^Not/{print "W: Not Connected"}{if ($1 == "SSID:") print "W: " $2}'
