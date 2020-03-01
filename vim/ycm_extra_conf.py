e2ef32436f17cc02edd678a17a48589617533bdd
Ashfaqur Rahman <sajib.finix@gmail.com>
2020-03-01 00:06:35 -0800

    Created new directory for vim configs

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -59,7 +59,7 @@ flags = [
 # compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
-'c',
+'c++',
 '-I',
 '/usr/include',
 '-isystem',
