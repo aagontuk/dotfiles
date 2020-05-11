26631d8a1314a6e9d208fa09e4381de922458c11
Ashfaqur Rahman <sajib.finix@gmail.com>
2020-05-11 14:51:23 -0700

    Added weather info

--- lemonbar.sh
+++ lemonbar.sh
@@ -7,6 +7,7 @@ while true; do
 	sts="${sts} $($SDIR/disk_status.sh) | "
 	sts="${sts} $($SDIR/net_status.sh) | "
 	sts="${sts} $($SDIR/bat_status.sh) | "
+    sts="${sts} $(curl -s wttr.in/Riverside?format=3) | "
 	sts="${sts} $(date '+%a %b-%d-%Y %I:%M:%S %p') "
     lsts="$(~/dotfiles/lemonbar/wspace_status.sh)"
 	echo "%{l}$lsts%{r}%{B#9A6324}%{F#000075}$sts"
